db6228f4e9f8c0f34e7481812bd7b81d65837006
David Pineiro <david@davidpineiro.xyz>
2022-05-03 00:30:20 -0400

    debugging...

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,19 +2,17 @@ package com.example.application;
 
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * The entry point of the Spring Boot application.
@@ -31,10 +29,15 @@ public class Application extends SpringBootServletInitializer implements AppShel
 
     public static void main(String[] args) {
         try {
-            URL applicationProp = Application.class.getClassLoader()
-                                    .getResource("application.properties");
+            InputStream applicationPropStream = Application.class.getClassLoader()
+                                    .getResourceAsStream("application.properties");
+
+            DataInputStream inputStream = new DataInputStream(applicationPropStream);
+            String propFileContent = new String(inputStream.readAllBytes());
+
+            inputStream.close();
             //before we start the application we must append the password into the application.properties file
-            String propFileContent = Files.readString(Paths.get(applicationProp.toURI()));
+//            String propFileContent = Files.readString(Paths.get(applicationProp.toURI()));
             if (propFileContent.contains("vaadin.productionMode=true")) { //cus lmao
                 String content = Files.readString(Path.of("/var/password/certpass.txt"));
                 String password = content.strip();//idk just incase
