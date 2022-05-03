c4d2fd4855b3372dfc14bc1804de90af35052441
David Pineiro <david@davidpineiro.xyz>
2022-05-02 23:35:36 -0400

    fixed it up lmao

---
+++ scripts/appendProperties.sh
@@ -0,0 +1,3 @@
+echo "vaadin.productionMode=true" >> src/main/resources/application.properties
+echo "server.ssl.enabled=true" >> src/main/resources/application.properties
+echo "server.ssl.key-store-password=$(cat /run/secrets/certpass.txt)" >> src/main/resources/application.properties
