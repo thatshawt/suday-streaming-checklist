94673edd92346b3dd8541a4ff81a20604518206a
David Pineiro <david@davidpineiro.xyz>
2022-05-04 01:45:15 -0400

    sploinky yoinky...

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,31 +1,27 @@
 package com.example.application.views.main;
 
-import com.example.application.ShartifyText;
-import com.vaadin.flow.component.button.Button;
+import com.example.application.elements.RickRoll;
+import com.example.application.elements.ShartifyText;
 import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 @PageTitle("Sunday Streaming Checklist")
 @Route(value = "")
 public class MainView extends VerticalLayout {
 
     private final ShartifyText title;
+    private final RickRoll roll;
     private final List<Checkbox> checks;
 
     public MainView() {
+        roll = new RickRoll("bruhj");
         title = new ShartifyText("Sunday Streaming Checklist");
         title.addClassName("unselect");
 
@@ -42,7 +38,7 @@ public class MainView extends VerticalLayout {
         add(new Paragraph("Impressive. Very nice. Let's see Paul Allen's card."));
         add(new Image("https://c.tenor.com/hi9RxkFRriAAAAAM/no-maidens-megamind.gif",
                 "no maidens?"));
-        add(new Image("https://c.tenor.com/bVh8C-q-TG4AAAAC/interstellar-cost.gif", "Yoinky Sploinky"));
+        add(new Image("https://c.tenor.com/bVh8C-q-TG4AAAAC/interstellar-cost.gif", "Sploinky Yoinky"));
         add(new Image("https://c.tenor.com/y0tNqpVBR9AAAAAd/thanos-avengers.gif", "Thanos, The Mad Titan"));
         add(new Image("https://c.tenor.com/J3qLu0yobXYAAAAC/thanos-philosophy.gif", "Strongest Will"));
         add(new Image("https://c.tenor.com/5bnBHE0rvtgAAAAC/infinity-war-avengers.gif", "~Everything..."));
