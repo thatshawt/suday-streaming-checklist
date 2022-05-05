eff8e5b6d4e1bddd398c246ce83298de70c7f4ef
David Pineiro <david@davidpineiro.xyz>
2022-05-04 21:23:07 -0400

    Merge branch '2-rickroller' into main
    # Conflicts:
    #	src/main/java/com/example/application/views/main/MainView.java

---
+++ src/main/java/com/example/application/DarkThemeHtml.java
@@ -0,0 +1,10 @@
+package com.example.application;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JavaScript;
+
+@Tag("div")
+@JavaScript("./darkMode.js")
+public class DarkThemeHtml extends Component {
+}
