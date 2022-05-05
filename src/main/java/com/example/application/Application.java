3fdccd0ad7314261a62ffdafc7649515f410ad48
David Pineiro <david@davidpineiro.xyz>
2022-05-04 21:23:20 -0400

    Merge branch '2-rickroller' into main
    # Conflicts:
    #	src/main/java/com/example/application/views/main/MainView.java

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,13 +1,14 @@
 package com.example.application;
-
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.DataInputStream;
+
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.InputStream;
@@ -22,15 +23,16 @@ import java.nio.file.Path;
  *
  */
 @SpringBootApplication
-@Theme(value = "mytodo")
 //@PWA(name = "My Todo", shortName = "My Todo", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
+@Theme(value = "mytodo")
+//@Theme(themeClass = Lumo.class, variant = Lumo.DARK)
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         try {
             InputStream applicationPropStream = Application.class.getClassLoader()
-                                    .getResourceAsStream("application.properties");
+                    .getResourceAsStream("application.properties");
 
             DataInputStream inputStream = new DataInputStream(applicationPropStream);
             String propFileContent = new String(inputStream.readAllBytes());
@@ -45,6 +47,16 @@ public class Application extends SpringBootServletInitializer implements AppShel
                 //thanks https://www.baeldung.com/properties-with-spring
                 System.setProperty("server.ssl.key-store-password", password);//finally
                 System.setProperty("server.ssl.enabled", "true");
+                System.setProperty("server.ssl.key-store","/run/secrets/certstore.p12");
+                /*
+                server.ssl.protocol=TLS
+                server.ssl.key-store-type=pkcs12
+                server.ssl.key-alias=springboot
+                */
+                System.setProperty("server.ssl.protocol","TLS");
+                System.setProperty("server.ssl.key-store-type","pkcs12");
+                System.setProperty("server.ssl.key-alias","springboot");
+
             }
         }catch(Exception e){
             e.printStackTrace();
@@ -54,3 +66,18 @@ public class Application extends SpringBootServletInitializer implements AppShel
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
